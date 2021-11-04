941fbc094f1f4cca03af97bf566837161dd258e2
LordKrymer <57912475+LordKrymer@users.noreply.github.com>
2021-11-04 01:28:44 -0300

    smarty abstraido de las funciones en la view

--- view/VehiculosView.php
+++ view/VehiculosView.php
@@ -1,33 +1,30 @@
 <?php
 require_once 'libs/smarty-3.1.39/libs/Smarty.class.php';
 class VehiculosView{
+    private $smarty;
 
     public function __construct(){
-
+        $this->smarty = new Smarty();
     }
 
     public function mostrarHome(){
-        $smarty = new Smarty();
-        $smarty->display('templates/home.tpl');
+        $this->smarty->display('templates/home.tpl');
     }
 
     public function mostrarAutos(){
-        $smarty = new Smarty();
-        $smarty->display('templates/autos.tpl');
+        
+        $this->smarty->display('templates/autos.tpl');
     }
 
     public function mostrarMotos(){
-        $smarty = new Smarty();
-        $smarty->display('templates/motos.tpl');
+        $this->smarty->display('templates/motos.tpl');
     }
 
     public function mostrarLuces(){
-        $smarty = new Smarty();
-        $smarty->display('templates/luces.tpl');
+        $this->smarty->display('templates/luces.tpl');
     }
 
     public function mostrarVideoJuegos(){
-        $smarty = new Smarty();
-        $smarty->display('templates/videojuegos.tpl');
+        $this->smarty->display('templates/videojuegos.tpl');
     }
 }
